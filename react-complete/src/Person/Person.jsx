2798b552a73c32deb8e2e76a33687fd1cd95c7e0
Cal <calvin.m71@hotmail.com>
2020-10-02 23:33:05 -0400

    css added

--- react-complete/src/Person/Person.jsx
+++ react-complete/src/Person/Person.jsx
@@ -1,13 +1,16 @@
 import React from "react";
+import "./Person.css";
 
-const Person = (props) => {
+const person = (props) => {
   return (
-    <div>
-      <h1>
+    <div className="Person">
+      <p onClick={props.click}>
         I'm {props.name} and I am {props.age} years old!
-      </h1>
+      </p>
+      <p>{props.children}</p>
+      <input type="text" onChange={props.changed} value={props.name} />
     </div>
   );
 };
 
-export default Person;
+export default person;
